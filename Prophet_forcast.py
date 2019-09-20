3e5d01cc1d7e78abc91cd995c0ab766d907cbf83
JDeanThomas <JDeanThomas#gmail.com>
2019-09-20 14:40:47 -0400

    Initial commit

---
+++ Prophet_forcast.py
@@ -0,0 +1,31 @@
+import fbprophet
+import pandas as pd
+from scipy.stats import boxcox
+from scipy.special import inv_boxcox
+
+# Create DF from API query
+df = datasets['plays_daily']
+
+df.shape
+df.dtypes
+
+df['ds'] = df['date']
+df['y'] = df['value']
+
+df.set_index('date')
+
+# Apply Box-Cox Transform to value column and assign to new column y
+df['y'], lam = boxcox(df['value'])
+
+model = fbprophet.Prophet()
+model.fit(df)
+
+future = model.make_future_dataframe(periods=365)
+forecast = model.predict(future)
+
+model.plot(forecast);
+model.plot_components(forecast);
+
+# Apply inverse Box-Cox transform to specific forecast columns
+forecast[['yhat', 'yhat_upper', 'yhat_lower']] = forecast[['yhat', 
+          'yhat_upper', 'yhat_lower']].apply(lambda x: inv_boxcox(x, lam))
---
+++ README.md
@@ -0,0 +1,3 @@
+#### Facebooks profit library:
+
+##### Making predictions of plays on Spotify of friend's band after a single release. 
